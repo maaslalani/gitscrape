570baa3d257a46acc49c0755ed7a4e4b9b16f764
Maas Lalani <maaslalani1@gmail.com>
2019-12-21 16:28:34 -0500

    Loading screen and table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import SearchIcon from './search.svg';
 import CodeIcon from './code.svg';
 import './App.css';
 
+const sampleData = [
+  {name: 'Maas Lalani', login: 'maaslalani', website: 'maaslalani.com', linkedin: 'https://linkedin.com/in/maaslalani'},
+  {name: 'Darshil Patel', login: 'darshil', website: 'darshilpatel.com', linkedin: 'https://linkedin.com/in/darshil36'},
+];
+
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
 function Search({search, setSearch, onSubmit}) {
   return (
     <div className="search-container">
@@ -37,12 +44,63 @@ function Logo() {
   );
 }
 
+function Table({data}) {
+  const tableHeaders = Object.keys(data[0]);
+
+  return (
+    <table className="table">
+      <tbody>
+        <tr>{tableHeaders.map(th => <th key={th}>{capitalize(th)}</th>)}</tr>
+        {data.map(row => <tr>{Object.values(row).map(column => <td>{column}</td>)}</tr>)}
+      </tbody>
+    </table>
+  );
+}
+
+function Download({onClick}) {
+  return (
+    <button className="download" onClick={onClick}>
+      Download
+    </button>
+  )
+}
+
 function App() {
   const [search, setSearch] = useState('');
+  const [results, setResults] = useState();
+  const [loading, setLoading] = useState(false);
 
   function onSubmit() {
-    
+    setResults(sampleData);
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
   }
+
+  const searchPage = (
+    <div className="content">
+      <Logo />
+      <Instructions />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+
+  const loadingPage = (
+    <div className="content">
+      <div className="loading" />
+      <p>Waiting for data, this may take some time.</p>
+    </div>
+  );
+
+  const resultsPage = loading ? loadingPage : (
+    <div className="result">
+      <Download />
+      <Table data={results} />
+    </div>
+  );
 
   return (
     <div className="App">
@@ -50,11 +108,7 @@ function App() {
         <div>Git Scrape</div>
         <div>View on Github</div>
       </nav>
-      <div className="content">
-        <Logo />
-        <Instructions />
-        <Search search={search} setSearch={setSearch} onSubmit={onSubmit} />
-      </div>
+      {results ? resultsPage : searchPage}
     </div>
   );
 }
--- src/api/app.js
+++
@@ -1,7 +0,0 @@
-import Github from 'octonode';
-
-const Client = Github.client();
-
-Client.get('/users/maaslalani', {}, function (err, status, body, headers) {
-  console.log(body);
-});
