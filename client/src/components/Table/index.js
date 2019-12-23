0522567eb54a37cd1d113bd5fbc83bd289afcd23
Maas Lalani <maaslalani1@gmail.com>
2019-12-23 12:38:33 -0500

    link to url if td is url

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -10,9 +10,23 @@ function Table({data}) {
     <table className="table">
       <tbody>
         <tr>{tableHeaders.map(th => <th key={th}>{capitalize(th)}</th>)}</tr>
-        {data.map((row, index) => <tr key={index}>{Object.values(row).map(column => <td key={column}>{column}</td>)}</tr>)}
+        {data.map((row, index) => <tr key={index}>{Object.values(row).map(column => <td key={column}>{isUrl(column) ? wrapLink(column) : column}</td>)}</tr>)}
       </tbody>
     </table>
+  );
+}
+
+function isUrl(string) {
+  if (!string) {
+    return false;
+  }
+
+  return string.includes('http');
+}
+
+function wrapLink(url) {
+  return (
+    <a target="_blank" href={url}>{url}</a>
   );
 }
 
