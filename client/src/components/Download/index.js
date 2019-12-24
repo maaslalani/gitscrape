85e4f97db9efd6f9195ac1565ec8719ee9a6d038
Maas Lalani <maaslalani1@gmail.com>
2019-12-23 20:59:52 -0500

    download csv and json working

--- client/src/components/Download/index.js
+++ client/src/components/Download/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import CSVDownloader from 'react-csv-downloader';
 import './main.css';
 
 function Download({data}) {
+  const json = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`;
   return (
     <div className="download-container">
-      <button download className="download-json">
+      <a className="download-json" href={json} download="gitscrape.json">
         Download JSON
-      </button>
-      <button download className="download-csv">
-        Download CSV
-      </button>
+      </a>
+      <div className="csv-downloader">
+        <CSVDownloader datas={data} filename="gitscrape">
+          <button className="download-csv">
+            Download CSV
+          </button>
+        </CSVDownloader>
+      </div>
     </div>
   )
 }
